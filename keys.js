9adafc11f850e91127d123e6a208f6bb7e766f90
aliriza <alirizakulla@ymail.com>
2021-09-06 21:09:09 +0200

    Changes

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    mongoURI: `mongodb+srv://adim_user:user123@cluster0.ud4lj.mongodb.net/library_db?retryWrites=true&w=majority`,
+    mongoURI: `mongodb+srv://admin_user:<password>@cluster0.ud4lj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     secretKey: '19945-62890-06876-52361'
 }
